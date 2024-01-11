9a18c62e0db22d63c0a62a1c9d8cfcd851009a91
Edgar Juan Guercio <jguercio@live.com.ar>
2024-01-11 18:16:06 -0300

    ADD vercel config

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -48,7 +48,7 @@ const ProductCard=(product)=>{
                     <button className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
                     onClick={checkout}
                     >              
-                      Comprar
+                      Pagar Compra
                     </button>
                   </div>
                 </div>   
